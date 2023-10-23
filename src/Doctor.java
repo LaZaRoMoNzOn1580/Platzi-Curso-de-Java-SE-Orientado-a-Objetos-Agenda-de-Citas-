f2ef2b6805f64662f40030630bece70f99627ce3
Lazaro Manuel <lmonzongonzalez2019@gmail.com>
2023-10-23 17:06:47 +0200

    Agregando una Clase Anidada a la Clase Doctor para crear citas disponibles

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
 
     static int id = 0;
-    String name;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     Doctor(String Name, String Speciality){
         this.name = Name;
@@ -17,6 +21,50 @@ public class Doctor {
 
     public void showId(){
         System.out.println(id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableApoointment(Date date,String time){
+        availableAppointments.add(new AvailableAppointment(date,time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment {
+        private int id_availableAppointment;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId_availableAppointment() {
+            return id_availableAppointment;
+        }
+
+        public void setId_availableAppointment(int id_availableAppointment) {
+            this.id_availableAppointment = id_availableAppointment;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -8,6 +10,13 @@ public class Main {
         Doctor docLarry = new Doctor("Larry","Dermatologo");
         Patient patLion = new Patient("Lion","lmonzongonzalez2019@gmail.com");
 
+        docLarry.addAvailableApoointment(new Date(),"4pm");
+        docLarry.addAvailableApoointment(new Date(),"1pm");
+        docLarry.addAvailableApoointment(new Date(),"10am");
+
+        for (Doctor.AvailableAppointment a: docLarry.getAvailableAppointments()) {
+            System.out.println(a.getDate() + " " + a.getTime());
+        }
 
     }
 

9ae21aa4866d69e6de64e894e531ed2a454ea86a
Lazaro Manuel <lmonzongonzalez2019@gmail.com>
2023-10-23 04:57:40 +0200

    Modificando la clase UIMenu en el metodo showPatientMenu

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,8 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctorLarry = new Doctor("Larry");
-        myDoctorLarry.showName();
-        myDoctorLarry.showId();
-
-        Doctor myDoctorLion = new Doctor("Lion");
-        myDoctorLion.showName();
-        myDoctorLion.showId();
-
         showMenu();
-
+        
     }
 
 
